a03602665e52ec9c0df3a62e51a7e8b5955acb8c
wizzyxx <spythemout@gmail.com>
2024-08-22 17:53:11 +0600

    update

--- resources/views/frontend/single_pages/works.blade.php
+++ resources/views/frontend/single_pages/works.blade.php
@@ -1,6 +1,17 @@
 @extends('frontend.layouts.app')
 @section('content')
-<link href="/technomole/public/css/styles.css" rel="stylesheet" />
+<meta http-equiv="x-ua-compatible" content="ie=edge">
+
+
+    <!-- CSS 
+    ============================================ -->
+    <link rel="stylesheet" href="/technomole/public/assets_works/css/vendor/bootstrap.min.css">
+    <link rel="stylesheet" href="/technomole/public/assets_works/css/vendor/slick.css">
+    <link rel="stylesheet" href="/technomole/public/assets_works/css/vendor/slick-theme.css">
+    <link rel="stylesheet" href="/technomole/public/assets_works/css/vendor/aos.css">
+    <link rel="stylesheet" href="/technomole/public/assets_works/css/plugins/feature.css">
+    <!-- Style css -->
+    <link rel="stylesheet" href="/technomole/public/assets_works/css/style.css">
 
 <style>
   /*! elementor - v3.5.3 - 28-12-2021 */
@@ -46,11 +57,206 @@
   display: inline-block
   }
 </style>
-<style>
+
+
+                                
+<section>
+      <!-- 4 LOREM IPSUM -->
+       <!-- Portfolio-->
+       <div class="template-color-1 spybody white-version" data-spy="scroll" data-bs-target=".navbar-example2" data-offset="150">
+          <!-- Start Portfolio Area -->
+          <div id="portfolio" class="rn-portfolio-area portfolio-style-three rn-section-gap section-separator">
+            <div class="container">
+
+                <div class="row mt--25 mt_md--5 mt_sm--5">
+                    <div class="col-lg-12">
+                        <div data-aos="fade-up" data-aos-duration="500" data-aos-delay="200" data-aos-once="true" class="portfolio-wrapper portfolio-slick-activation slick-arrow-style-one rn-slick-dot-style">
+
+                            <!-- Start Single Portfolio  -->
+                            @if($works->count()>=1)
+                            <div class="rn-portfolio-slick">
+                                <div class="rn-portfolio" data-bs-toggle="modal" data-bs-target="#exampleModalCenter">
+                                    <div class="inner">
+                                        <div class="thumbnail">
+                                            <a href="javascript:void(0)">
+                                                <img src="{{url('public/upload/images/'.@$works['0']->image)}}" alt="Personal Portfolio Images">
+                                            </a>
+                                        </div>
+                                        <div class="content">
+                                            <div class="category-info">
+                                                <div class="category-list">
+                                                    <a href="javascript:void(0)">PHOTOSHOP</a>
+                                                </div>
+                                                <div class="meta">
+                                                    <span><a href="javascript:void(0)"><i class="feather-heart"></i></a>
+                                                        650</span>
+                                                </div>
+                                            </div>
+                                            <h4 class="title"><a href="javascript:void(0)">{{@$works['0']->title}}<i class="feather-arrow-up-right"></i></a></h4>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            @endif
+                            <!-- End Single Portfolio  -->
+
+                            <!-- Start Single Portfolio  -->
+                            @if($works->count()>=2)
+                            <div class="rn-portfolio-slick">
+                                <div class="rn-portfolio" data-bs-toggle="modal" data-bs-target="#exampleModalCenter">
+                                    <div class="inner">
+                                        <div class="thumbnail">
+                                            <a href="javascript:void(0)">
+                                                <img src="{{url('public/upload/images/'.@$works['1']->image)}}" alt="Personal Portfolio Images">
+                                            </a>
+                                        </div>
+                                        <div class="content">
+                                            <div class="category-info">
+                                                <div class="category-list">
+                                                    <a href="javascript:void(0)">Figma</a>
+                                                </div>
+                                                <div class="meta">
+                                                    <span><a href="javascript:void(0)"><i class="feather-heart"></i></a>
+                                                        650</span>
+                                                </div>
+                                            </div>
+                                            <h4 class="title"><a href="javascript:void(0)">{{@$works['1']->title}}<i class="feather-arrow-up-right"></i></a></h4>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            @endif
+                            <!-- End Single Portfolio  -->
+
+                            <!-- Start Single Portfolio  -->
+                            @if($works->count()>=3)
+                            <div class="rn-portfolio-slick">
+                                <div class="rn-portfolio" data-bs-toggle="modal" data-bs-target="#exampleModalCenter">
+                                    <div class="inner">
+                                        <div class="thumbnail">
+                                            <a href="javascript:void(0)">
+                                                <img src="{{url('public/upload/images/'.@$works['2']->image)}}" alt="Personal Portfolio Images">
+                                            </a>
+                                        </div>
+                                        <div class="content">
+                                            <div class="category-info">
+                                                <div class="category-list">
+                                                    <a href="javascript:void(0)">Laravel</a>
+                                                </div>
+                                                <div class="meta">
+                                                    <span><a href="javascript:void(0)"><i class="feather-heart"></i></a>
+                                                        650</span>
+                                                </div>
+                                            </div>
+                                            <h4 class="title"><a href="javascript:void(0)">{{@$works['2']->title}}<i class="feather-arrow-up-right"></i></a></h4>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            @endif
+                            <!-- End Single Portfolio  -->
+
+                            <!-- Start Single Portfolio  -->
+                            @if($works->count()>=4)
+                            <div class="rn-portfolio-slick">
+                                <div class="rn-portfolio" data-bs-toggle="modal" data-bs-target="#exampleModalCenter">
+                                    <div class="inner">
+                                        <div class="thumbnail">
+                                            <a href="javascript:void(0)">
+                                                <img src="{{url('public/upload/images/'.@$works['3']->image)}}" alt="Personal Portfolio Images">
+                                            </a>
+                                        </div>
+                                        <div class="content">
+                                            <div class="category-info">
+                                                <div class="category-list">
+                                                    <a href="javascript:void(0)">Figma</a>
+                                                </div>
+                                                <div class="meta">
+                                                    <span><a href="javascript:void(0)"><i class="feather-heart"></i></a>
+                                                        650</span>
+                                                </div>
+                                            </div>
+                                            <h4 class="title"><a href="javascript:void(0)">{{@$works['3']->title}}<i class="feather-arrow-up-right"></i></a></h4>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            @endif
+                            <!-- End Single Portfolio  -->
+
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- End portfolio Area -->
+
       
-    </style>
-	<!-- mfn_hook_content_before -->
-     <div id="Content">
+         <!-- Modal Portfolio Body area Start -->
+         <div class="modal fade" id="exampleModalCenter" tabindex="-1" role="dialog" aria-hidden="true">
+            <div class="modal-dialog modal-dialog-centered" role="document">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <button type="button" class="close" data-bs-dismiss="modal" aria-label="Close">
+                            <span aria-hidden="true"><i data-feather="x"></i></span>
+                        </button>
+                    </div>
+                    <div class="modal-body">
+                        <div class="row align-items-center">
+
+                            <div class="col-lg-6">
+                                <div class="portfolio-popup-thumbnail">
+                                    <div class="image">
+                                        <img class="w-100" src="{{url('public/upload/images/'.@$works['0']->image)}}" alt="slide">
+                                    </div>
+                                </div>
+                            </div>
+
+                            <div class="col-lg-6">
+                                <div class="text-content">
+                                    <h3>
+                                        <span>Featured - Design</span> {{@$works['0']->title}}
+                                    </h3>
+                                    <p class="mb--30">{!! @$works['0']->description !!}
+                                    <div class="button-group mt--20">
+                                        <a href="https://amarhaor.com" class="rn-btn">
+                                            <span>VIEW PROJECT</span>
+                                            <i data-feather="chevron-right"></i>
+                                        </a>
+                                    </div>
+
+                                </div>
+                                <!-- End of .text-content -->
+                            </div>
+                        </div>
+                        <!-- End of .row Body-->
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- End Modal Portfolio area -->
+       
+    </main>
+    <!-- JS ============================================ -->
+    <script src="/technomole/public/assets_works/js/vendor/jquery.js"></script>
+    <script src="/technomole/public/assets_works/js/vendor/modernizer.min.js"></script>
+    <script src="/technomole/public/assets_works/js/vendor/feather.min.js"></script>
+    <script src="/technomole/public/assets_works/js/vendor/slick.min.js"></script>
+    <script src="/technomole/public/assets_works/js/vendor/bootstrap.js"></script>
+    <script src="/technomole/public/assets_works/js/vendor/text-type.js"></script>
+    <script src="/technomole/public/assets_works/js/vendor/wow.js"></script>
+    <script src="/technomole/public/assets_works/js/vendor/aos.js"></script>
+    <script src="/technomole/public/assets_works/js/vendor/particles.js"></script>
+    <script src="/technomole/public/assets_works/js/vendor/jquery-one-page-nav.js"></script>
+    <!-- main JS -->
+    <script src="/technomole/public/assets_works/js/main.js"></script>
+</div>
+<!--/Portfolio-->
+
+        </section>
+
+        	<!-- mfn_hook_content_before -->
+    <div id="Content">
         <div class="content_wrapper clearfix">
            <div class="sections_group">
               <div class="entry-content" itemprop="mainContentOfPage">
@@ -60,71 +266,7 @@
                        <div class="the_content_wrapper is-elementor">
                           <div data-elementor-type="wp-page" data-elementor-id="67" class="elementor elementor-67" data-elementor-settings="[]">
                              <div class="elementor-section-wrap">
-      <!-- 4 LOREM IPSUM -->
-       <!-- Portfolio-->
-           <!-- Portfolio-->
-           <section class="content-section" id="portfolio">
-            <div class="container px-4 px-lg-5">
-                <div class="content-section-heading text-center">
-                    <!--<h3 class="text-secondary mb-0">Portfolio</h3>
-                    <h2 class="mb-5">Recent Projects</h2>-->
-                </div>
-                <div class="row gx-0">
-                @if($works->count()>=1)
-                    <div class="col-lg-6">
-                        <a class="portfolio-item" href="#!">
-                            <div class="caption">
-                                <div class="caption-content">
-                                    <div class="h2">{{@$works['0']->title}}</div>
-                                    <p class="mb-0"><?php echo substr(@$works['0']->description,0,240); ?></p>
-                                </div>
-                            </div>
-                            <img class="img-fluid" src="{{url('public/upload/images/'.@$works['0']->image)}}" alt="..." />
-                        </a>
-                    </div>
-                    @endif
-                    @if($works->count()>=2)
-                    <div class="col-lg-6">
-                        <a class="portfolio-item" href="#!">
-                            <div class="caption">
-                                <div class="caption-content">
-                                    <div class="h2">{{@$works['1']->title}}</div>
-                                    <p class="mb-0"><?php echo substr(@$works['1']->description,0,240); ?></p>
-                                </div>
-                            </div>
-                            <img class="img-fluid" src="{{url('public/upload/images/'.@$works['1']->image)}}" alt="..." />
-                        </a>
-                    </div>
-                    @endif
-                    @if($works->count()>=3)
-                    <div class="col-lg-6">
-                        <a class="portfolio-item" href="#!">
-                            <div class="caption">
-                                <div class="caption-content">
-                                    <div class="h2">{{@$works['2']->title}}</div>
-                                    <p class="mb-0"><?php echo substr(@$works['2']->description,0,240); ?></p>
-                                </div>
-                            </div>
-                            <img class="img-fluid" src="{{url('public/upload/images/'.@$works['2']->image)}}" alt="..." />
-                        </a>
-                    </div>
-                    @endif
-                    @if($works->count()>=4)
-                    <div class="col-lg-6">
-                        <a class="portfolio-item" href="#!">
-                            <div class="caption">
-                                <div class="caption-content">
-                                    <div class="h2">{{@$works['3']->title}}</div>
-                                    <p class="mb-0"><?php echo substr(@$works['3']->description,0,240); ?></p>
-                                </div>
-                            </div>
-                            <img class="img-fluid" src="{{url('public/upload/images/'.@$works['3']->image)}}" alt="..." />
-                        </a>
-                    </div>
-                    @endif
-                </div>
-            </div>
-        </section>
+
       
                                 <!--//4 LOREM IPSUM -->
                                 <!-- OUR SPECIALISTS -->
